29fb104b6974a7dd79b3777e5dac90a3819c9507
Alex-Key05 <cgamer143@gmail.com>
2021-01-07 15:24:14 -0600

    Avance del proyecto

---
+++ cliente/src/components/proyectos/Proyectos.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Proyectos = () => {
+    return (
+        <div>
+            <h1>Adiós desde Proyectos</h1>
+        </div>
+    )
+}
+
+export default Proyectos
